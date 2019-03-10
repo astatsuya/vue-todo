f22b0eb2f9b8fee77a3ad23d557fc03657f8c0d5
astatsuya <sharekobekko@gmail.com>
2019-03-10 20:10:03 +0900

    remove extra contents

--- src/components/Hello.vue
+++ src/components/Hello.vue
@@ -1,14 +1,9 @@
 <template>
   <div>
     {{ msg }}
-    <InputArea
-      :new-todo="newTodo"
-      @input="input"
-      @addTodo="addTodo"
-      @removeTodo="removeTodo"
-    />
-    <ToggleArea @changeShowTodo="changeShowTodo" />
-    <TaskList :todos="todos" :show-todo="showTodo" />
+    <InputArea />
+    <ToggleArea />
+    <TaskList />
   </div>
 </template>
 <script>
@@ -25,43 +20,8 @@ export default {
   },
   data: function() {
     return {
-      msg: "Welcome to my Todo",
-      todos: [
-        { id: 1, text: "vue-router", done: false },
-        { id: 2, text: "vuex", done: false },
-        { id: 3, text: "vue-loader", done: false },
-        { id: 4, text: "awsome-vue", done: false },
-        { id: 5, text: "vue-router", done: true }
-      ],
-      newTodo: "",
-      showTodo: "all"
+      msg: "Welcome to my Todo"
     };
-  },
-  methods: {
-    input: function(child) {
-      this.newTodo = child;
-    },
-    addTodo: function() {
-      let text = this.newTodo && this.newTodo.trim();
-      if (!text) {
-        return;
-      }
-      const id = this.todos.slice(-1)[0].id + 1;
-      this.todos.push({
-        id: id,
-        text: text,
-        done: false
-      });
-      this.newTodo = "";
-    },
-    removeTodo: function() {
-      for (let i = this.todos.length - 1; i >= 0; i--) {
-        if (this.todos[i].done) this.todos.splice(i, 1);
-      }
-    },
-    changeShowTodo: function(e) {
-      this.showTodo = e;
-    }
   }
 };
 </script>
